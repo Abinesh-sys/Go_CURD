db0e06b3c5941c559d58dcb31151b2bfd5cd912c
Abinesh09 <149653551+Abinesh09@users.noreply.github.com>
2025-01-30 22:37:40 +0530

    Connect database in main.go

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main 
 
-import "fmt"
+import (
+	"fmt"
+	"gocurd/database"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	database.Connect()
+
+	fmt.Println("Successfully Connected")
 }
